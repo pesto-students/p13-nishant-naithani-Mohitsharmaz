d88ab0eb7383210b19d2c052036a6469c3d2c7d2
mohitsharmaz <mohitsharmabhardwaj9630@gmail.com>
2023-08-27 11:15:14 +0530

    assignment week3

--- Week-3/assignment3/password.js
+++ Week-3/assignment3/password.js
@@ -1,11 +1,14 @@
-const checkPass =(password)=>{
-    
-    if(password.length >8 && !password.includes("password") && password !== password.toLowerCase() ){
-        console.log("strong password")
-        return true
-    }else{
-        console.log("weak password")
-        return false
-    }
-}
-checkPass("Mohit@123")
+const checkPass = (password) => {
+  if (
+    password.length > 8 &&
+    !password.includes("password") &&
+    password !== password.toLowerCase()
+  ) {
+    console.log("strong password");
+    return true;
+  } else {
+    console.log("weak password");
+    return false;
+  }
+};
+checkPass("Mohit@123");
